fe610568c081c6d1086c711039f5eefa7ae3571a
juan <callesjuan@gmail.com>
2015-03-19 16:00:58 -0300

    implementing periodically camera shots

--- app/src/main/java/br/ufes/inf/lprm/ninjabubble/br/ufes/inf/lprm/ninjabubble/views/HomeView.java
+++ app/src/main/java/br/ufes/inf/lprm/ninjabubble/br/ufes/inf/lprm/ninjabubble/views/HomeView.java
@@ -4,6 +4,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +21,13 @@ import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import br.ufes.inf.lprm.ninjabubble.R;
 
@@ -38,6 +49,10 @@ public class HomeView extends ContentView {
     public final long LOOKUP_INTERVAL = 1000 * 10;
     public Dialog mLookupDialog;
     private double mLookupRadius = 0.015; // aprox 1.67km radius, where in 0.03 is aprox 3.34km and reta da penha has aprox 3km length
+
+    public Timer mTimer;
+    public final long TIMER_RATE = 1000 * 10;
+    public Camera mCamera;
 
     public HomeView(Context context, final OverlayView overlayView) {
         super(context, overlayView);
@@ -739,5 +754,87 @@ public class HomeView extends ContentView {
                 }
             }
         });
+    }
+
+    public void startTimer() {
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+                && ((Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP))) {
+            // your code here - is between15-21
+
+            mCamera = Camera.open();
+            try {
+                Camera.Parameters parameters = mCamera.getParameters();
+
+                List<Camera.Size> sizes = parameters.getSupportedPictureSizes();
+                Camera.Size lowestSize = null;
+                for (Camera.Size size : sizes) {
+                    if (lowestSize == null || (size.width <= lowestSize.width && size.height <= lowestSize.height)) {
+                        lowestSize = size;
+                    }
+                }
+
+                parameters.setPictureSize(lowestSize.width, lowestSize.height);
+
+                mCamera.setPreviewDisplay(null);
+                mCamera.startPreview();
+
+                mTimer = new Timer();
+                mTimer.scheduleAtFixedRate(new MyTimerTask(), TIMER_RATE, TIMER_RATE);
+
+            } catch (Exception e) {}
+
+        } else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+            // your code here - is api 21
+        }
+    }
+
+    public void stopTimer() {
+        try {
+            mTimer.cancel();
+            mTimer = null;
+
+            mCamera.stopPreview();
+            mCamera.release();
+        } catch (Exception e) {}
+    }
+
+    public class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            mCamera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        }
+
+        Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
+            public void onShutter() {
+                Log.d(TAG, "onShutter'd");
+            }
+        };
+
+        Camera.PictureCallback rawCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Log.d(TAG, "onPictureTaken - raw");
+            }
+        };
+
+        Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                FileOutputStream outStream = null;
+                try {
+                    // write to local sand box file system
+                    //outStream = CameraDemo.this.openFileOutput(String.format("%d.jpg", System.currentTimeMillis()), 0);
+                    // Or write to s d card
+                    outStream = new FileOutputStream(String.format("/sdcard/%d.jpg", System.currentTimeMillis()));
+                    outStream.write(data);
+                    outStream.close();
+                    Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {}
+                Log.d(TAG, "onPictureTaken - jpeg");
+            }
+        };
     }
 }
